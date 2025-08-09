4cc5eaed8aadd4f62f00bfe87dbd5422e69c700d
sksksk2024 <cota8091@gmail.com>
2025-08-09 12:56:56 +0300

    Last stretch before sharing it

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import type { Metadata } from 'next';
-import './globals.css';
-import { Toaster } from 'sonner';
-import { AudioProvider } from '@/components/contexts/AudioContext';
-import { ClerkProvider } from '@clerk/nextjs';
+import type { Metadata } from "next";
+import "./globals.css";
+import { Toaster } from "sonner";
+import { AudioProvider } from "@/components/contexts/AudioContext";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-  title: 'Beestrong',
+  title: "Beestrong",
   description:
-    'Forta divina, direct de la albina! Nu conteaza cat de slab ai inceput, cu noile produse de la Beestrong n-ai cum sa nu te simti puternic!!!',
+    "Forta divina, direct de la albina! Nu conteaza cat de slab ai inceput, cu noile produse de la Beestrong n-ai cum sa nu te simti puternic!!!",
 };
 
 export default function RootLayout({
@@ -19,16 +19,20 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" className="bg-black font-title">
         <head>
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link rel="preconnect" href="https://js.stripe.com" />
-        <link rel="icon" href="/beestrongIcon.svg" sizes="32x32" />
-        <link rel="apple-touch-icon" href="/beestrongIcon.svg" sizes="32x32" />
-      </head>
-        <body className={`antialiased font-specter`}>
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link rel="preconnect" href="https://js.stripe.com" />
+          <link rel="icon" href="/beestrongIcon.svg" sizes="32x32" />
+          <link
+            rel="apple-touch-icon"
+            href="/beestrongIcon.svg"
+            sizes="32x32"
+          />
+        </head>
+        <body id="bg" className={`antialiased font-specter`}>
           <AudioProvider>
             {children}
             <Toaster />
